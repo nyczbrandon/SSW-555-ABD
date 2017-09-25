63527948ca770a57d4d64ac3fca64bd7baa77a5a
O803 <nyczbrandon@gmail.com>
2017-09-24 21:47:27 -0400

    Fix tests

--- cs555/test/gedcom/GEDCOMAgeLimitTest.java
+++ cs555/test/gedcom/GEDCOMAgeLimitTest.java
@@ -12,16 +12,33 @@ public class GEDCOMAgeLimitTest {
 	@BeforeClass
 	public static void before() throws Exception {
 		gr = new GEDCOMReader( "resources/project01.ged" );
+		gr.setAgeLimit();
 	}
 	
 	@Test
-	public void test() {
+	public void test1() {
+		assertNotNull( gr.getFile() );
+	}
+	
+	@Test
+	public void test2() {
 		assertNotNull( gr.getIndividuals() );
+	}
+	
+	@Test
+	public void test3() {
 		assertNotNull( gr.getFamilies() );
-		gr.setAgeLimit();
+	}
+	
+	@Test
+	public void test4() {
+		assertEquals( gr.AGE_LIMIT, 150);
+	}
+	
+	@Test
+	public void test5() {
 		for ( Map.Entry<String, Individual> e : gr.getIndividuals().entrySet() ) {
 			assertTrue( e.getValue().getAge() < 150 );
 		}
 	}
-
 }
