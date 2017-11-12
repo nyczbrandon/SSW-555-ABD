2d08967600bacc2bdc9dabb8fb7b1ae6e616d35a
O803 <nyczbrandon@gmail.com>
2017-11-12 00:32:22 -0500

    Add checking recent births and deaths

---
+++ cs555/test/gedcom/GEDCOMCheckRecentDeathsTest.java
@@ -0,0 +1,39 @@
+package gedcom;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class GEDCOMCheckRecentDeathsTest {
+	private static GEDCOMReader gr;
+	
+	@BeforeClass
+	public static void before() throws Exception {
+		gr = new GEDCOMReader( "resources/BrandonCheungSSW555Project01.ged" );
+	}
+	
+	@Test
+	public void test1() {
+		assertNotNull( gr.getFile() );
+	}
+	
+	@Test
+	public void test2() {
+		assertNotNull( gr.getIndividuals() );
+	}
+	
+	@Test
+	public void test3() {
+		assertNotNull( gr.getFamilies() );
+	}
+	
+	@Test
+	public void test4() {
+		List<String> errors = gr.checkRecentDeaths();
+		assertTrue( errors.size() > 0 );
+	}
+}
